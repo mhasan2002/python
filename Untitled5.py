313fb339420808fe0215b1ae60178777022547b6
mhasan2002 <90141049+mhasan2002@users.noreply.github.com>
2021-10-24 01:38:23 +0500

    aaa

---
+++ Untitled5.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from math import pi
+r = float(input ("Input the radius of the circle : "))
+print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
+
+
+# In[ ]:
+
+
+irstname = input("What is your last name?:")
+lastname = input("What's your last?: ")
+print  (lastname)  +  (firstname)[::-1]
+
+
+# In[ ]:
+
+
+firstname = input("What is your first name?:")
+   lastname = input("What's your last name?: ")
+   fn = firstname[::-1]
+   ln = lastname[::-1]  
+   print (fn,"",ln)
+
+
+# In[ ]:
+
+
+print("Twinkle , twinkle little star, \n \t how  wonder what you are\n\t\t up above the world so high\n\t\tlike a diamond in the sky\n twinkle twinke little star\n \t how i wonder what you are ")
+
+
+# In[ ]:
+
+
+ListName = [ "hasan", "ashraf", 1,2,3 ]
+print ("The list is : " + str(ListName))
+counter = 0
+for i in ListName:
+counter = counter + 1
+print ("Length of list using naive method is : " + str(counter))
+
+
+# In[ ]:
+
+
+from operator import length_hint 
+inp_lst = ['Python','Java','Kotlin','Machine Learning','Keras']
+print("Length of the input string:")
+size = length_hint(inp_lst)
+print(size)
+
+
+# In[ ]:
+
+
+list = [19, 10, 45, 26, 6]
+print("Largest number of the list is:", max(list))
+
+
+# In[ ]:
+
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+for i in a:
+if i < 5: 
+print(i)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
